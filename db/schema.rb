17f0981b5ada4011be6395ff7bc506bd651d3a6a
squareballer <squarjn@gmail.com>
2015-09-26 12:48:27 -0400

    Started working on order creation page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920190930) do
+ActiveRecord::Schema.define(version: 20150921030810) do
 
   create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
